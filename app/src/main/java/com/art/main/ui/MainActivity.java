87dbcd8a4514557944999bc509bcd450939e8eac
liuxian <215856591@qq.com>
2019-06-26 18:21:03 +0800

    优化资源；

---
+++ app/src/main/java/com/art/main/ui/MainActivity.java
@@ -0,0 +1,18 @@
+package com.art.main.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @Author lx
+ * @Date 2019/6/26
+ * @Desc
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
